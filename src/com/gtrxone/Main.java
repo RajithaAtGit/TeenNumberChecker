8d8262784e27ef00835d884c3ea7bc86d3661fab
Goonawardana R N W <gtrxone@gmail.com>
2021-11-16 10:29:31 +0530

    Initial commit

---
+++ src/com/gtrxone/Main.java
@@ -0,0 +1,9 @@
+package com.gtrxone;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(TeenNumberChecker.hasTeen(22,14,34));
+        System.out.println(TeenNumberChecker.isTeen(13));
+    }
+}
